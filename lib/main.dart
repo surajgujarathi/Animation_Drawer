594a15fc53abf6abb8550994becf8d3570291b37
surajgujarathi <surajsuri1989@gmail.com>
2024-07-24 11:57:29 +0530

    Animated drawer

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:animation_drawer/drawer_screen.dart';
+import 'package:animation_drawer/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+     home: Scaffold(
+      body: Stack(
+        children: [
+          DrawerScreen(),
+          HomeScreen(),
+        ],
+      ),
+     ),
+
+  
+    );
+  }
+}
